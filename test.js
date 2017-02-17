8761b0e1e74a079b02e94821e02b097265927c61
timeinfeldt <tim.einfeldt@gmail.com>
2017-02-17 23:37:40 +0100

    asdasd

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 function readWriteSync() {
   var newValue = 'AS)GFASUGUASGUSAUGU';
 
-  fs.writeFileSync('./output.txt', newValue, 'utf-8');
+  fs.writeFileSync('output.txt', newValue, 'utf-8');
 
   console.log('readFileSync complete');
 }
