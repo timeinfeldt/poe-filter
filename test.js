b50160a4dc6d6f6a7f9f2dc9bee3eaaf8f00d96d
timeinfeldt <tim.einfeldt@gmail.com>
2017-02-17 23:46:49 +0100

    __dirname

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 function readWriteSync() {
   var newValue = 'NEW VALUE';
 
-  fs.writeFileSync('./output.txt', newValue, 'utf-8');
+  fs.writeFileSync(__dirname + '/output.txt', newValue, 'utf-8');
 
   console.log('readFileSync complete');
 }
