328599333a0b1b6cc3e2e0808651942685bfea63
timeinfeldt <tim.einfeldt@gmail.com>
2017-02-18 00:05:55 +0100

    file path

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ function readWriteSync() {
 
   var newValue = 'new value';
 
-  fs.writeFileSync('output.txt', newValue, 'utf-8');
+  fs.writeFileSync('./output.txt', newValue, 'utf-8');
 
   console.log('readFileSync complete');
 }
