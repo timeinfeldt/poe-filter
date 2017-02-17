662ecd81c16808c56ad6ba376cc0783cc78a78e9
timeinfeldt <tim.einfeldt@gmail.com>
2017-02-18 00:26:56 +0100

    fsdfsdfd

--- deploy.js
+++ deploy.js
@@ -1,7 +1,9 @@
+console.log('deploy script');
+
 var fs = require('fs');
 
 function readWriteSync() {
-  var data = fs.readFileSync('./output.txt', 'utf-8');
+  var data = fs.readFileSync(__dirname + '/output.txt', 'utf-8');
   console.log(data);
 }
 
