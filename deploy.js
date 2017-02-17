e8f8991b4de351881232533d9014bcc791a34b94
timeinfeldt <tim.einfeldt@gmail.com>
2017-02-18 00:23:25 +0100

    asdasd

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 function readWriteSync() {
-  var data = fs.readFileSync('output.txt', 'utf-8');
+  var data = fs.readFileSync('./output.txt', 'utf-8');
   console.log(data);
 }
 
