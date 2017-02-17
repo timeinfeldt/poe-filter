5e6eb68929d273f0b491ef36272a87a02e5d5b99
timeinfeldt <tim.einfeldt@gmail.com>
2017-02-17 23:11:46 +0100

    alter

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 
 function readWriteSync() {
+  var newValue = 'AS)GFASUGUASGUSAUGU';
+
   fs.writeFileSync('./output.txt', newValue, 'utf-8');
 
   console.log('readFileSync complete');
