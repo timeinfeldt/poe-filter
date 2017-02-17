cd99486b2319f1d41431c46f583c0105630006e9
timeinfeldt <tim.einfeldt@gmail.com>
2017-02-18 00:47:15 +0100

    asdad

--- deploy.js
+++ deploy.js
@@ -1,10 +1,25 @@
 console.log('deploy script');
-
+var request = require('request');
 var fs = require('fs');
 
 function readWriteSync() {
   var data = fs.readFileSync(__dirname + '/output.txt', 'utf-8');
-  console.log(data);
+  submitData();
+}
+
+
+function submitData() {
+
+
+request.post(
+    'https://hooks.zapier.com/hooks/catch/1988398/mmzf0m/',
+    { json: { key: 'value' } },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+        }
+    }
+);
 }
 
 readWriteSync();
