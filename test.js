30f503aa53fed9beeaab25cce7215061413385d0
timeinfeldt <tim.einfeldt@gmail.com>
2017-02-18 00:55:12 +0100

    asdasd

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 function readWriteSync() {
 
-  var newValue = 'NEW VALUE IN FILE';
+  var newValue = 'NEW VALUE IN FILE1';
 
   fs.writeFileSync('./output.txt', newValue, 'utf-8');
 
