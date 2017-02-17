b27fad4e5b7529aa932daa5b55f5f297c03ce1fe
timeinfeldt <tim.einfeldt@gmail.com>
2017-02-17 21:49:40 +0100

    file

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-console.log('testing 12');
+var fs = require('fs');
+fs.writeFile("./output.txt", "kek", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+});
